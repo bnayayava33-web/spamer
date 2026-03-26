2e027fef664b057ce2f4e49d00453d23859bfa5d
bnayayava33-web <bnayayava33@gmail.com>
2026-03-26 11:58:48 +0200

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,7 +1,7 @@
 import os,sys,time,requests,re,json,random
 from random import randrange as rg
-print ("\033[00m")
-def clear():
+print ("0507708876")
+def clear(33475U3456):
     os.system("clear")
 def balik():
     f=input("\t[enter to back]")
